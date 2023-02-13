2d91be9c1124d6a62b59834fb6f0498b05956db0
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-13 11:47:09 +0100

    Найти индекс искомого значеня в массиве

---
+++ Method_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,22,31,4,5,64,7,8,4};
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] ==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
