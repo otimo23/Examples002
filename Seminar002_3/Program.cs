3a8f4a666de6b95ac6d5f3ff08e74ae5e36af0fc
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-14 11:31:04 +0100

    два числа на входе,кратны или нет.Если нет выводит остаток

---
+++ Seminar002_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Write first number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Write second number: ");
+int num2 = int.Parse(Console.ReadLine()!);
+if(num1%num2 ==0)
+{
+    Console.WriteLine($"{num1}, {num2} - kratno");
+}
+else
+{
+    int reminder = num1 % num2; 
+    Console.WriteLine($"{num1}, {num2} - ne kratno, ostatok = {reminder} ");
+}
+/*12 Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если второе число не кратно числу первому,
+то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно*/
