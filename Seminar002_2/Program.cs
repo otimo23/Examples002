899101dce1629a2e29e13ba30259bfac36e2d1d1
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-14 11:27:28 +0100

    выводит случайное трёхзначное число и удаляет вторую цифру

---
+++ Seminar002_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int num = new Random().Next (100, 1000);
+Console.WriteLine(num); // 223
+int a1 = num / 100;
+int a2 = num % 10;
+int a11 = a1 * 10;
+int result = a11 + a2;
+Console.WriteLine($"Модифицированное число от {num} -> {result}");
+/* 11 Напишите программу, которая выводит случайное 
+трёхзначное число и удаляет вторую цифру этого числа*/
+
+
+
