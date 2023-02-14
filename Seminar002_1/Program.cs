c2d3a461cd929edf68e98dfe19d3dfe7e18d8605
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-14 11:23:46 +0100

    Найти максимальную цифру рандомного числа

---
+++ Seminar002_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int num = new Random().Next(10, 100);
+int a1 = num / 10; 
+int a2 = num % 10; 
+int max = a1;
+if (max < a2)
+    max = a2;
+Console.WriteLine($"Max of {num} -> {max}");
+/* Напишите программу, которая выводит случайное число из отрезка
+// [10, 99] и показывает наибольшую цифру числа.*/
+
