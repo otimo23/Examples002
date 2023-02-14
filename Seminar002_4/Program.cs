f673c13f1e93b219ec11929f8730bd11a657f590
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-14 11:32:47 +0100

    принимает на вход число и проверяеткратно ли оно одновременно 7 и 23

---
+++ Seminar002_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Write first number: ");
+int num = int.Parse(Console.ReadLine()!);
+int numA = 7;
+int numB = 23;
+
+if(num % numA ==0 && num % numB ==0)
+{
+    Console.WriteLine($"{num} - kratno 7 and 23");
+}
+else
+{
+ 
+    Console.WriteLine($"{num} - ne kratno 7 and 23");
+}
+/*Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.*/
