86417b7f4e5323d2e55539e06c135b72c780a936
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-14 11:34:22 +0100

    принимает на вход два числа и проверяет,является ли одно число квадратом другого

---
+++ Seminar002_5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Write first number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Write second number: ");
+int num2 = int.Parse(Console.ReadLine()!);
+if(num1 * num1 == num2 || num1 / num2 == num2 )
+{
+    Console.WriteLine($"{num1}, {num2} - da");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2} - net ");
+}
+ /*Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.*/
+
+
+
+
+
+
+
+
